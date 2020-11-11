d4d6b1fc37158703de738745c9a011c46853bb1b
Nhan Tran <nhan.tran2@tiki.vn>
2020-11-11 13:46:29 +0700

    sort recording

--- src/pages/upload-management/services/index.js
+++ src/pages/upload-management/services/index.js
@@ -11,34 +11,35 @@ import { LIMIT } from '../constants';
 // }
 
 export async function getUploadedList(params) {
-  const page = get(params, 'page', 1)
-  const limit = get(params, 'limit', LIMIT)
+  const page = get(params, 'page', 1);
+  const limit = get(params, 'limit', LIMIT);
 
   return request('/recording', {
     method: 'GET',
     prefix: 'http://ec2-13-250-23-149.ap-southeast-1.compute.amazonaws.com',
     params: {
-      $skip: (page - 1) * limit
-    }
+      $skip: (page - 1) * limit,
+      '$sort[id]': -1,
+    },
   });
 }
 
 export async function getUploadedDetail(id) {
-  return request('/recording' + id, {
+  return request(`/recording/${id}`, {
     method: 'GET',
     prefix: 'http://ec2-13-250-23-149.ap-southeast-1.compute.amazonaws.com',
   });
 }
 
 export async function getTranscript(params) {
-  const page = get(params, 'page', 1)
-  const limit = get(params, 'limit', LIMIT)
+  const page = get(params, 'page', 1);
+  const limit = get(params, 'limit', LIMIT);
 
   return request('/transcript', {
     method: 'GET',
     prefix: 'http://ec2-13-250-23-149.ap-southeast-1.compute.amazonaws.com',
     params: {
-      $skip: (page - 1) * limit
-    }
+      $skip: (page - 1) * limit,
+    },
   });
 }
