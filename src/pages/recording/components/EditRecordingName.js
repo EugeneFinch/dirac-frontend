7c8bbf56ea8d941c982741cf4b00deacb288a5bb
Donald <trung.nguyen@2359media.com>
2021-03-31 09:48:20 +0700

    fix: UI edit recording subject

--- src/pages/recording/components/EditRecordingName.js
+++ src/pages/recording/components/EditRecordingName.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Input, Row } from 'antd';
+import {Col, Input, Row} from 'antd';
+import {CheckCircleTwoTone, CloseCircleTwoTone, EditOutlined} from "@ant-design/icons";
 
 const EditRecordingName = ({ id, name, recording, putRecording }) => {
   const [edit, setEdit] = useState(false);
@@ -22,12 +23,35 @@ const EditRecordingName = ({ id, name, recording, putRecording }) => {
     setInput(target.value)
   }
 
+  if (edit) {
+    return (<Row gutter={15} justify="start" align="middle">
+      <Col>
+        <Input style={{ width: 180 }} bordered={false}
+               defaultValue={input}
+               value={input}
+               onPressEnter={submit}
+               onChange={onChangeRecordingName}
+               onClick={onClickRecordingName}
+        />
+      </Col>
+      <Col>
+        <CloseCircleTwoTone
+          style={{ fontSize: 25 }}
+          onClick={(e) => { e.stopPropagation(); setEdit(false) }}
+          twoToneColor="#eb2f96"
+        />{' '}
+        <CheckCircleTwoTone style={{ fontSize: 25 }} onClick={submit} twoToneColor="#52c41a" />
+      </Col>
+    </Row>
+    );
+  }
+
   return (
-    <Row gutter={15} justify="start" align="middle">
-      { edit ? <Input autoFocus defaultValue={input} onPressEnter={submit} onClick={onClickRecordingName} onChange={onChangeRecordingName}></Input> : (
-        <label defaultValue={input} onClick={onClickRecordingName}>{input}
-        </label>
-      )}
+    <Row gutter={5}>
+      <Col onClick={onClickRecordingName}>{input}</Col>
+      <Col>
+        <EditOutlined onClick={onClickRecordingName} />
+      </Col>
     </Row>
   );
 };
