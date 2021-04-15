026cb26ccee7267bc6cc3cb26a66251133fe4d7d
Donald <trung.nguyen@2359media.com>
2021-04-15 23:39:28 +0700

    fix: route

--- src/pages/analytic/services/index.js
+++ src/pages/analytic/services/index.js
@@ -1,7 +1,7 @@
 import request from '@/utils/request';
 
 export async function getTalkToListenList({ userId }) {
-  return request(`/analytics-talk-to-listen-export?userId=${userId}`, {
+  return request(`/analytics-talk-to-listen?userId=${userId}`, {
     method: 'GET',
   });
 }
